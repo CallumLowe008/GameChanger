2f2c71971c2a565bc08ca87d98558d4380882ac8
Zombee0804 <samb8424@gmail.com>
2022-01-22 12:03:14 +0000

    Added basic player movement
    Currently, the controls don't change

---
+++ src/GameChangerPrototype/Assets/Scripts/ControlManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlManager : MonoBehaviour
+{
+    public Dictionary<string, KeyCode>[] keyMap = new Dictionary<string, KeyCode>[] {
+        new Dictionary<string, KeyCode>(),
+        new Dictionary<string, KeyCode>()
+    };
+
+    void Start() {
+        keyMap[0].Add("up", KeyCode.W);
+        keyMap[0].Add("right", KeyCode.A);
+        keyMap[0].Add("left", KeyCode.S);
+        keyMap[0].Add("down", KeyCode.D);
+        keyMap[0].Add("stop", KeyCode.LeftShift);
+
+        keyMap[1].Add("up", KeyCode.UpArrow);
+        keyMap[1].Add("right", KeyCode.RightArrow);
+        keyMap[1].Add("left", KeyCode.LeftArrow);
+        keyMap[1].Add("down", KeyCode.DownArrow);
+        keyMap[1].Add("stop", KeyCode.RightControl);
+    }
+
+    public List<KeyCode> GetKeysInUse() {
+        List<KeyCode> keys = new List<KeyCode>();
+
+        foreach (Dictionary<string, KeyCode> player in keyMap) {
+            foreach (KeyCode key in player.Values) {
+                keys.Add(key);
+            }
+        }
+
+        return keys;
+    }
+}
---
+++ src/GameChangerPrototype/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("References")]
+    public Rigidbody2D body;
+    public ControlManager controlManager;
+    public int id;
+
+    [Header("Movement")]
+    public float moveSpeed;
+    public int direction;
+    public int stopper;
+
+    void Start() {
+        direction = 1;
+    }
+
+    void Update() {
+
+        KeyCode value;
+        if (controlManager.keyMap[id].TryGetValue("stop", out value)) {
+            if (Input.GetKey(value)) {
+                stopper = 0;
+            }
+            else {
+                stopper = 1;
+            }
+        }   
+
+        body.position += new Vector2(moveSpeed * direction, 0) * Time.deltaTime * stopper;
+    }
+}
