4b56a21ef687ed14f3840ba3ab0d8b43038333ca
Zombee0804 <samb8424@gmail.com>
2022-01-26 09:28:07 +0000

    Added Jump Pads
    Reorganised script folder & slightly tweaked level

---
+++ src/GameChangerPrototype/Assets/Scripts/GameMechanics/JumpPad.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpPad : MonoBehaviour
+{
+    public float jumpForce;
+
+    void OnCollisionEnter2D(Collision2D col) {
+        if (col.gameObject.tag == "Player") {
+            col.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+}
