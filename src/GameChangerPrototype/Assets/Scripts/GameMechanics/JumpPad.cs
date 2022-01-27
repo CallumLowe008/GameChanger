beed0f373b9978d03d9efeea1b6ac26cc5af7ccc
Zombee0804 <samb8424@gmail.com>
2022-01-27 22:42:27 +0000

    Started work on jump pad trajectory
    Jump pads now show a trajectory based on the jump force and player movement. It currently only works for jump pads on the 'floor' and 'ceiling', not ones that are on the 'walls'.

--- src/GameChangerPrototype/Assets/Scripts/GameMechanics/JumpPad.cs
+++ src/GameChangerPrototype/Assets/Scripts/GameMechanics/JumpPad.cs
@@ -6,6 +6,39 @@ public class JumpPad : MonoBehaviour
 {
     public float jumpForce;
 
+    [Header("Trajectory Plotting")]
+    public PlayerMovement playerMovement;
+    public int horizontalFacing;
+    public int verticalFacing;
+    public float trajectoryDrawIterations;
+    public float trajectoryDrawIncrement;
+
+    void OnDrawGizmos() {
+
+        if (trajectoryDrawIterations <= 0 || trajectoryDrawIncrement <= 0 || playerMovement == null) {
+            return;
+        }
+
+        Vector3 playerHeight = Vector3.up * playerMovement.transform.localScale.y/2;
+
+        // Calculate Velocity
+        Vector2 velocity = (Vector2.right*horizontalFacing*playerMovement.moveSpeed) + (Vector2.up*verticalFacing*jumpForce);
+
+        // Draw Line
+        Gizmos.color = Color.green;
+        for (float i = 0; i < (trajectoryDrawIterations-trajectoryDrawIncrement); i += trajectoryDrawIncrement) {
+            Gizmos.DrawLine(
+                PlotTrajectoryAtTime(transform.position, velocity, i) + playerHeight, 
+                PlotTrajectoryAtTime(transform.position, velocity, i += trajectoryDrawIncrement) + playerHeight
+            );
+        }
+    }
+
+    Vector3 PlotTrajectoryAtTime(Vector3 startPos3, Vector2 velocity, float time) {
+        Vector2 startPos = new Vector2(startPos3.x, startPos3.y);
+        return startPos + (velocity*time) + (Physics2D.gravity*time*time*0.5f*verticalFacing);
+    }
+
     void OnCollisionEnter2D(Collision2D col) {
         if (col.gameObject.tag == "Player") {
             col.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
