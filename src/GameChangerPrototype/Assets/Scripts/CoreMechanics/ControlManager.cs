f9b79d7bf749a8ba59a248f68aee27250a3252b1
Zombee0804 <samb8424@gmail.com>
2022-02-01 18:48:14 +0000

    Fixed collision bug

--- src/GameChangerPrototype/Assets/Scripts/CoreMechanics/ControlManager.cs
+++ src/GameChangerPrototype/Assets/Scripts/CoreMechanics/ControlManager.cs
@@ -10,6 +10,7 @@ public class ControlManager : MonoBehaviour
     public Dictionary<string, SpriteRenderer> keyVisuals = new Dictionary<string, SpriteRenderer>();
 
     [Header("General")]
+    public RotationManager rotationManager;
     public bool disableKeyChanges;
 
     [Header("Key Icons")]
@@ -52,7 +53,6 @@ public class ControlManager : MonoBehaviour
     }
 
     public KeyCode UpdateKey(string keyName) {
-
         if (disableKeyChanges == true) {
             return KeyCode.None;
         }
