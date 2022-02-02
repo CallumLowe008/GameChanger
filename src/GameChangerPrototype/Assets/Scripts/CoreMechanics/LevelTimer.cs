3e1e2c9d928b4036fb01d04e53993c4a1b8a08b4
Zombee0804 <samb8424@gmail.com>
2022-02-02 09:24:11 +0000

    Added level timer

---
+++ src/GameChangerPrototype/Assets/Scripts/CoreMechanics/LevelTimer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelTimer : MonoBehaviour
+{
+    public float elapsedTime;
+    public string timerFormatted;
+    public Text timerDisplay;
+
+    void Update() {
+        elapsedTime += Time.deltaTime;
+        timerFormatted = FormatTime();
+        timerDisplay.text = timerFormatted;
+    }
+
+    string FormatTime() {
+        float minutes = Mathf.Floor(elapsedTime / 60);
+        float seconds = Mathf.Floor(elapsedTime - (minutes * 60));
+        float milliseconds = Mathf.Round((elapsedTime - (minutes * 60) - seconds) * 1000f);
+        return $"{minutes}:{seconds}:{milliseconds}";
+    }
+}
