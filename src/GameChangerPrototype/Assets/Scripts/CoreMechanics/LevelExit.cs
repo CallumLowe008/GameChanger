fd841f0049f5b48c935b877039f764cc2decfb32
Zombee0804 <samb8424@gmail.com>
2022-02-02 11:52:25 +0000

    Added level exit
    Also a temporary menu and end screen

---
+++ src/GameChangerPrototype/Assets/Scripts/CoreMechanics/LevelExit.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    public string sceneToLoad;
+
+    void OnCollisionEnter2D(Collision2D col) {
+        if (col.gameObject.tag == "Player") {
+            SceneManager.LoadSceneAsync(sceneToLoad);
+        }
+    }
+}
---
+++ src/GameChangerPrototype/Assets/Scripts/CoreMechanics/LevelManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    void Awake() {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void LoadLevel(string sceneToLoad) {
+        SceneManager.LoadSceneAsync(sceneToLoad);
+    }
+}
